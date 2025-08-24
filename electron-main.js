4a2bb7ddcdcb358aecdd5881485b8c90e8e8f473
Matti Kjellstadli <40216910+mattkje@users.noreply.github.com>
2025-08-24 11:58:22 +0200

    Initial commit

---
+++ electron-main.js
@@ -0,0 +1,27 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  // Load Vite dev server in development
+  win.loadURL('http://localhost:5173');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
